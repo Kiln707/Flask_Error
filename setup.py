10fe9fb9571c93af919a3a60009dacdb2b2b24b3
kiln707 <steven.guy.swanson@gmail.com>
2019-05-06 13:21:51 -0700

    Renames due to name collision

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from codecs import open
+from os import path
+
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as readme:
+    long_description = readme.read()
+
+setup(
+    name='Flask Error',
+    version='1.0.0',
+    description='Easy error code and messages with useful troubleshooting and callback features.',
+    long_description=long_description,
+    url='https://github.com/Kiln707/Flask_Error',
+    author='Kiln707',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='flask errors exceptions',
+    packages=find_packages(),
+    setup_requires='pytest-runner',
+    tests_require=[
+        'pytest',
+        'pytest-cov'
+    ],
+    install_requires=[
+        'flask'
+    ],
+    python_requires='>=3.5.*'
+)
