29b9f107661a1bc7102a7e7c3896b11ecf49d79e
kiln707 <steven.guy.swanson@gmail.com>
2019-10-31 16:40:26 -0700

    removed extra debugging stuff

--- test/test_error.py
+++ test/test_error.py
@@ -5,7 +5,6 @@ import sys
 def test_callback(app):
     errorcode = 500
     def callback_test(code, error, trace, request, **kwargs):
-        print(code, error, trace, request,**kwargs)
         assert code == errorcode
     @app.route('/500')
     def test_route1():
@@ -19,11 +18,11 @@ def test_callback(app):
     def test_route3():
         abort(404)
 
-    error = FlaskError(app, callback=[callback_test])
+    FlaskError(app, callback=[callback_test])
     client = app.test_client()
     try:
-        print(client.get('/500', follow_redirects=True))
-        print(client.get('/exception', follow_redirects=True))
+        client.get('/500', follow_redirects=True)
+        client.get('/exception', follow_redirects=True)
         client.get('/404', follow_redirects=True)
     except:
         print("Unexpected error:", sys.exc_info()[0])
